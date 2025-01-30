9ae0dc06ecc0b81c1bdeba79f2216ab14129961c
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-01-30 16:06:11 +0530

    create server.ts file

---
+++ server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import customerRoutes from "./routes/customer-routes";
+
+const app = express();
+
+app.use(express.json());
+app.use('/',(req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
+
+    next();
+})
+app.use('/customer',customerRoutes);
+
+app.listen(3001, (err=>{
+    console.log("Server running on port 3001");
+}));
+
+app.use('/',(req,res,next)=>{
+    res.status(200).send('Not Found');
+})
